c7f6d3c6df24257bef909fee9d760b57c51dcfa4
nicothebrush <nicola.riolini@gmail.com>
2018-04-18 11:15:54 +0200

    generic bugfix (fast message)

--- excel_export/__init__.py
+++ excel_export/__init__.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-import xlsx_utility
+from . import xlsx_utility
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
