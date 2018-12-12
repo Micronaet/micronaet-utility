423ca4a25caf1fe94ecf09bec9d021bcd371711e
nicothebrush <nicola.riolini@gmail.com>
2018-12-12 17:55:54 +0100

    generic bugfix (fast message)

--- excel_export/xlsx_utility.py
+++ excel_export/xlsx_utility.py
@@ -83,7 +83,6 @@ class ExcelWriter(models.Model):
     def _create_workbook(self, extension='xlsx'):
         ''' Create workbook in a temp file
         '''
-        import pdb; pdb.set_trace()
         now = fields.Datetime.now()
         now = now.replace(':', '_').replace('-', '_').replace(' ', '_')
         filename = '/tmp/wb_%s.%s' % (now, extension)
